c97414babf6d2599bff3c56701ab45de863edb4c
diky <diky@diky-PC>
2020-10-17 14:00:32 +0700

    Upload Project

---
+++ src/Logika.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 
+ * NAMA  : Diva Sabila Ramadhan 
+ * KELAS : IF1
+ * NIM   : 10119309
+ * Deskripsi Program : Program Logika 
+ */
+public class Logika {
+
+   
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa: " + dewasa);
+    }
+    
+}
